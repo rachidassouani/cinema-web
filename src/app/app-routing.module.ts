300039af90806ab1b959e3482d12e38789d675fd
rachidassouani <rachidassouani@gmail.com>
2020-06-03 22:18:49 +0100

    added cinema component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CinemaComponent } from './cinema/cinema.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cinema', component: CinemaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent
   ],
   imports: [
     BrowserModule,
